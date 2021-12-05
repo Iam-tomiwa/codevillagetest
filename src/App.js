8572b087e1695356e3287fd1555af16a7e4a68ff
Maryam Awesu <ajayiemmanuel630@gmail.com>
2021-12-05 21:04:43 +0100

    project init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+import TestCard from "./components/TestCard";
+import "./app.scss";
 
-function App() {
+const App = () => {
+  const [products, setProducts] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const abortCont = new AbortController();
+
+    const fetchProducts = async () => {
+      try {
+        setIsPending(true);
+        const res = await fetch("https://fakestoreapi.com/products?limit=10");
+        let data = await res.json();
+        setProducts(data);
+      } catch (err) {
+        console.log(err.message);
+        setError(err.message);
+      } finally {
+        setIsPending(false);
+      }
+    };
+
+    fetchProducts();
+
+    // abort the fetch
+    return () => abortCont.abort();
+  }, []);
+
+  console.log(products);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App layout">
+      <main className="inner">
+        <h1>New Arrivals</h1>
+        <section className="products-wrap">
+          {isPending ? (
+            <p>Loading...</p>
+          ) : error ? (
+            <p>{error}</p>
+          ) : (
+            products.map(product => (
+              <TestCard key={product.id} product={product} />
+            ))
+          )}
+        </section>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/utils/functions.js
@@ -0,0 +1,8 @@
+export const formatNumberToCurrency = (number, currency = "USD") => {
+  if (number === undefined) return 0;
+  number = Number(number);
+  return Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: currency,
+  }).format(number);
+};
